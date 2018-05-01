7149a989b7c7867ed797665e13ce94574e8de0b5
Anton <scoutrul@mail.ru>
2018-05-02 00:38:50 +0300

    form and some css

--- assets/app/src/js/script.js
+++ assets/app/src/js/script.js
@@ -48,36 +48,60 @@ $(function() {
 	toggleUIbyScreenWidth();
 
 	$('#send_form_button').on('click', function() {
+		var ModalForm = $('#send-form');
 		var name = $('#your_name').val();
 		var tel = $('#your_tel').val();
 		var email = $('#your_email').val();
 		var theme = $('#your_theme').val();
 		var msg = $('#your_message').val();
+		var modalFooter = $('.modal-footer');
+		var formGroup = $('.form-group');
 
 		var dataString =
 			'fromName=' + name + '&tel=' + tel + '&email=' + email + '&subject=' + theme + '&message=' + msg;
 
-		console.log('sending', dataString);
+		//show loading
+		
+		var OK = (name && email && msg) || false;
+		
+		if (OK) {
+			ModalForm.append("<div id='loader'></div>");
+			$('#loader').addClass('loader');
 
-		$.ajax({
-			type: 'POST',
-			url: 'feedback.html',
-			data: dataString,
-			dataType: 'html',
-			success: function() {
-				$('#send-form').html("<div id='message_success'></div>");
-				$('#message_success')
-					.html('<h2>Сообщение отправлено!</h2>')
-					.append('<p>Скоро мы с вами свяжемся</p>')
-					.hide()
-					.fadeIn(1500, function() {
-						$('#message_success').append('<i class="icon-ok"></i>');
+			formGroup.hide();
+			modalFooter.hide();
+			$('.modal-title').hide();
+			$('#send_form').hide();
+			$.ajax({
+				type: 'POST',
+				url: 'feedback.html',
+				data: dataString,
+				dataType: 'html',
+				success: function() {
+					$('#loader').removeClass('loader');
+					ModalForm.html("<div id='message_success'></div>");
+					$('#message_success')
+						.hide()
+						.fadeIn(1500, function() {
+							$('#message_success').append('<i class="icon-ok"></i>');
+						})
+						.html('<h2>Отправлено!</h2>')
+						.append('<p>Скоро мы с вами свяжемся</p>');
+
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown) {
+					ModalForm.removeClass('loader');
+					ModalForm.html("<div id='message_success'></div>");
+					$('#message_success').hide().fadeIn(1500, function() {
+						$('#message_success')
+							.append('<i class="icon-ok"></i>')
+							.html('<h2>Техническая ошибка!</h2>')
+							.append('<p>Пожалуйста, повторите позже или свяжитесь с нами по телефонам</p>');
 					});
-				$('.modal-title').hide();
-				$('#send_form').hide();
-			}
-		});
+					
+				}
+			});
+		}
 		return false;
 	});
-
 });
